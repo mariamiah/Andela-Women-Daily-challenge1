8555a7c589c2d1b19d90240c4204c7a370fa6eb8
= <=>
2018-09-14 20:01:16 +0300

    Modify test_dictionary file

--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -1,15 +1,21 @@
-from dictionary import returnDict, numbers
+from dictionary import returnDict
 import unittest
 
 
 class TestDict(unittest.TestCase):
+    def setUp(self):
+        self.numbers = returnDict()
+         
     def test_return_dict(self):
-        self.assertEqual(len(numbers), 15)
+        self.assertEqual(len(self.numbers), 15)
     
     def test_keys(self):
-        self.assertEqual(numbers[1], 1)
-        self.assertEqual(numbers[2], 4)
-        self.assertEqual(numbers[5], 25)
+        self.assertEqual(self.numbers[1], 1)
+        self.assertEqual(self.numbers[2], 4)
+        self.assertEqual(self.numbers[5], 25)
 
     def test_numbers_type(self):
-        self.assertIsInstance(numbers, dict)
+        self.assertIsInstance(self.numbers, dict)
+    
+    def testReturndict(self):
+        self.assertEqual(returnDict(), self.numbers)
